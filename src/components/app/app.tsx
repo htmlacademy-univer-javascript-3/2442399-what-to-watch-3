613b9c772aa018dd2ab4b93e9507e581803e2b80
MarkovGerman <german.markov@urfu.me>
2023-10-06 19:16:52 +0500

    2.1. Создание страниц и компонентов

---
+++ src/components/app/app.tsx
@@ -0,0 +1,15 @@
+import MainScreen from '../../pages/main-screen/main-screen';
+
+type AppScreenProps = {
+    title: string;
+    genre: string;
+    releaseDate: number;
+}
+
+function App({ title, genre, releaseDate }: AppScreenProps): JSX.Element {
+  return (
+    <MainScreen title={title} genre={genre} releaseDate={releaseDate} />
+  );
+}
+
+export default App;
